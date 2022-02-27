0dbbb871934db65d6fb72998aaaae89ee396f446
WildUrti <46956225+WildUrti@users.noreply.github.com>
2022-02-27 13:59:31 +0200

    design and code review form

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -173,7 +173,7 @@ public class Service {
             }
         }
         notaFileRepository.save(nota);
-        String filename = "fisiere/" + student.getNume() + ".txt";
+        String filename = "src/main/resources/fisiere/" + student.getNume() + ".txt";
         try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filename, true))){
             bufferedWriter.write("\nTema: " + tema.getID());
             bufferedWriter.write("\nNota: " + nota.getNota());
