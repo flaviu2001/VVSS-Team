1d5503c013013cf20ee986f04bcecd6a3b7cf35e
WildUrti <46956225+WildUrti@users.noreply.github.com>
2022-04-02 17:29:26 +0300

    wbt validator

--- src/test/java/validation/TemaValidatorTest.java
+++ src/test/java/validation/TemaValidatorTest.java
@@ -6,17 +6,54 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TemaValidatorTest {
+    TemaValidator tv = new TemaValidator();
+
     @Test
-    void validate1() {
-        TemaValidator tv = new TemaValidator();
-        Tema tema = new Tema("nr1", "grea tema", 2, 1);
-        assertDoesNotThrow(() -> tv.validate(tema));
+    void tc1(){
+        Tema tema = new Tema("", "d", 1, 1);
+        assertThrows(ValidationException.class, () -> tv.validate(tema));
     }
 
     @Test
-    void validate2() {
-        TemaValidator tv = new TemaValidator();
-        Tema tema = new Tema("", "grea tema", 2, 1);
+    void tc2(){
+        Tema tema = new Tema(null, "d", 1, 1);
         assertThrows(ValidationException.class, () -> tv.validate(tema));
+    }
+
+    @Test
+    void tc3(){
+        Tema tema = new Tema("1", "", 1, 1);
+        assertThrows(ValidationException.class, () -> tv.validate(tema));
+    }
+
+    @Test
+    void tc4(){
+        Tema tema = new Tema("1", "d", 0, 1);
+        assertThrows(ValidationException.class, () -> tv.validate(tema));
+    }
+
+    @Test
+    void tc5(){
+        Tema tema = new Tema("1", "d", 15, 1);
+        assertThrows(ValidationException.class, () -> tv.validate(tema));
+    }
+
+    @Test
+    void tc6(){
+        Tema tema = new Tema("1", "d", 1, 0);
+        assertThrows(ValidationException.class, () -> tv.validate(tema));
+    }
+
+    @Test
+    void tc7(){
+        Tema tema = new Tema("1", "d", 1, 15);
+        assertThrows(ValidationException.class, () -> tv.validate(tema));
+    }
+
+    @Test
+    void tc8() {
+        TemaValidator tv = new TemaValidator();
+        Tema tema = new Tema("1", "d", 1, 1);
+        assertDoesNotThrow(() -> tv.validate(tema));
     }
 }
